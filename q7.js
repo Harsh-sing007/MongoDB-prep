d75887f1dccca460ff2cae3063179c1567f0de3e
Harsh <harshsingh45831@gmail.com>
2026-02-02 12:35:28 +0530

    new commands

--- q7.js
+++ q7.js
@@ -4,3 +4,34 @@ db.employees.updateOne(
     {email:"cathy@gmail.com"},
     {$set:{salary:3500}}
 )
+
+db.employees.updateOne(
+    {email:"cathy@gmail.com"},
+    {$inc:{salary:3500}}    //increment
+)
+
+db.employees.updateMany(
+    {},
+    {$inc:{salary:1000}}    
+)  
+
+db.employees.updateMany(
+    {},
+    {$set:{points:1}}    
+)
+
+db.employees.updateOne(
+    {
+        email:"brain@gmail.com"},
+        {$set:{points:10}}
+);
+
+
+db.employees.updateOne(
+    {
+        email:"abc@gmail.com"},
+        {$set:{points:10}},
+        {upsert:true}
+);
+
+db.employees.find({},{name:1})
