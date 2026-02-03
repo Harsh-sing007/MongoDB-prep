d9b3c6573b9a335c411c5ab9157dfab0270ef389
Harsh <harshsingh45831@gmail.com>
2026-02-03 12:10:47 +0530

    aggregate commands

--- q11.js
+++ q11.js
@@ -6,3 +6,31 @@ db.employees.aggregate(
     ]
 )
 
+
+
+db.employees.aggregate(
+    [
+        {
+            $match: {department: "HR"}                           //Raw data → processing → meaningful output  .PIPELINE
+        },
+    ]
+)
+
+
+
+db.employees.aggregate(
+    [
+        {
+            $project: {_id:0,name: 1}                       
+        },
+    ]
+)
+
+
+db.employees.aggregate(
+    [
+        {
+            $project: {_id:0,department: 0,salary:0}                       
+        },
+    ]
+)
