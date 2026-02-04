b86ec46a6b267b37fa51a8b45770aa95a1d81cfb
Harsh <harshsingh45831@gmail.com>
2026-02-04 12:41:21 +0530

    address

--- q14.js
+++ q14.js
@@ -1,4 +1,11 @@
 db.employees.find(
     {},
-    {}
+    {name:1,dept:"$department"}             //displaying dept intsead of department
 )
+
+
+db.users.insertOne({
+    name:"Yah",
+    age:20,
+    address:{addr1:"50 Verdin Ct",city:"New York",state:"NY"},
+})
