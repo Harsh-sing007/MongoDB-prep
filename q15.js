e9e6de4e08fa0b04f3345c640d03b104bb6f5857
Harsh <harshsingh45831@gmail.com>
2026-02-04 14:29:32 +0530

    employees salary aggregate update

--- q15.js
+++ q15.js
@@ -1,12 +1,58 @@
 //employees and salary using aggregate
 
+db.employees.aggregate([
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      salary: 1,
+      grade: {
+        $cond: [
+          { $gt: ["$salary", 4500] },
+          "Grade A",
+          "Grade B"
+        ]
+      }
+    }
+  }
+])
+
+// employees and salary using aggregate with if-else
+db.employees.aggregate([
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      salary: 1,
+      grade: {
+        $cond: {
+          if: { $gte: ["$salary", 4500] },
+          then: "Grade A",
+          else: "Grade B"
+        }
+      }
+    }
+  }
+])
+
+
+//switch operator
+
 db.employees.aggregate(
     [
-        {$project:{
+    {
+        $project:{
             _id:0,
             name:1,
             salary:1,
-        }
-    }
-]
-)
+            grade:{
+                    $switch:{
+                        branches:[{case:{ $gt: ["$salary", 4500] },then:"Grade A"},{case:{ $gt: ["$salary", 4500] },then:"Grade B"}],
+                        default:"Grade C",
+                    },
+                    },
+ },
+  },
+]);
+
+
