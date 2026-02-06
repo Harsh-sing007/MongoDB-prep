77651039509e4772abfb2e5667279584812f522f
Harsh <harshsingh45831@gmail.com>
2026-02-06 10:27:09 +0530

    update

--- q22.js
+++ q22.js
@@ -9,3 +9,13 @@ db.employees.find({
 db.employees.find({
     name:{$regex:"John", $options:"i"}
 })
+
+//regex is case sensitive by default, we can make it case insensitive using options
+
+db.employees.find({
+    name:{$regex:"h$", $options:"i"}
+})
+
+db.employees.find({
+    name:{$regex:"^M", $options:"i"}
+})
