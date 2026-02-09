1d39507172639c07763c6e5574cbfd7b015c7bfc
Harsh <harshsingh45831@gmail.com>
2026-02-09 11:32:05 +0530

    update

---
+++ q25.js
@@ -0,0 +1,33 @@
+//Create  folder dbshards
+//create folders conf, confir, s1, s1r, s2, s2r
+//inside dbshards
+//Note: these  6 folders represent servers
+//located in different locations or countries
+//mongod --configsvr --replSet cf --dbpath "D:\dbshards\conf" --port 27018
+ //mongod --configsvr --replSet cf --dbpath "D:\dbshards\confir" --port 27019
+ //mongosh --port 27018
+
+rs.initiate({
+  _id: "cf",
+  members: [
+    { _id: 0, host: "127.0.0.1:27018" },
+    { _id: 1, host: "127.0.0.1:27019" }
+  ]
+})
+//rs.config()
+//rs.status()
+//mongod --shardsvr --replSet s1 --dbpath "D:\dbshards\s1" --port 27020
+//mongod --shardsvr --replSet s1 --dbpath "D:\dbshards\s1r" --port 27021
+//mongosh --port 27020
+rs.initiate({
+  _id: "s1",
+  members: [
+    { _id: 0, host: "127.0.0.1:27020" },
+    { _id: 1, host: "127.0.0.1:27021" }
+  ]
+})
+//rs.config()
+//rs.status()
+//mongod --shardsvr --replSet s2 --dbpath "D:\dbshards\s2" --port 27022
+//mongod --shardsvr --replSet s2 --dbpath "D:\dbshards\s2r" --port 27023
+//mongosh --port 27022
