53b0ea870f7a3484f5f9e39b20f9b62031f7b3fd
Harsh <harshsingh45831@gmail.com>
2026-02-09 11:36:05 +0530

    updated

--- q24.js
+++ q24.js
@@ -5,3 +5,26 @@ session.startTransaction()
 var custCollection = session.getDatabase("hdfcbank").customers    
 
 db.custCollection.updateOne({_id:"c1"},{$inc:{balance:-100}})
+
+
+
+db.courses.aggregate([
+  {
+    $lookup: {
+      from: "modules",
+      let: { courseId: "$_id" },
+      pipeline: [
+        { $match: { $expr: { $eq: ["$courseId", "$$courseId"] } } },
+        {$lookup:{
+          from:"lessons",
+          let:{moduleId:"$_id"},
+          pipeline:[
+            {$match:{$expr:{$eq:["$moduleId","$$moduleId"]}}}
+          ],
+          as:"lessons"
+        }}
+      ],
+      as: "modules",
+    },
+  },
+]);
